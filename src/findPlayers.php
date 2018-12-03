c711adc5875908ff2cc613d68b2d977a07f8c2bf
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2018-12-03 04:18:27 -0500

    Find player

--- src/findPlayers.php
+++ src/findPlayers.php
@@ -7,10 +7,10 @@ include('config.php');
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="PHP-Blackjack">
+    <meta name="description" content="PHP-Blackjack Find Players">
     <meta name="author" content="Anthony Whitaker">
 
-    <title>PHP-Blackjack</title>
+    <title>Find Players</title>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"

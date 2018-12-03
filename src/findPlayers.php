5ffb4dbab6120b2b8e8bfefec20cc40ddce7f734
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2018-12-03 15:38:08 -0500

    Add bgrd

--- src/findPlayers.php
+++ src/findPlayers.php
@@ -22,6 +22,7 @@ $players = searchPlayers($term, $database);
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
--- src/profile.php
+++ src/profile.php
@@ -28,7 +28,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="css/navbar.css">
-
+    <link rel="stylesheet" href="css/style.css">
+    
 </head>
 <body>
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
